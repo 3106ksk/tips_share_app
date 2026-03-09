d236f2b462dc561e4b74c4d4ad542e6d08265a28
Satoru kamiko <kiritaro973@gmail.com>
2026-03-09 15:21:13 +0900

    feat:スキーマ再設計とシードを使ってカテゴリーの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+categories = [
+  "インプット",
+  "アウトプット戦略",
+  "AI活用・プロンプト集",
+  "エラー解決・デバッグ戦略",
+  "おすすめアーカイブ動画"
+]
+
+categories.each do |category_name|
+  Category.find_or_create_by!(name: category_name)
+end
