91126fff72f9c6f4d107f12b6c4231ffdc16a5db
Satoru kamiko <kiritaro973@gmail.com>
2026-03-28 11:06:54 +0900

    style:フラッシュメッセージにアニメーション追加
    修正点
    - 次の処理を実行しないとUIとして残るをのアニメーションの表現で消えた風に作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :success then "bg-sky-50"
+    when :alert  then "bg-red-50"
+    else "bg-gray-500"
+    end
+  end
 end
+
